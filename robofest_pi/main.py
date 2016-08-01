aa25ef4ffc057cf581e3d6f23269b7382f432659
Oshan Mudannayake <oshan.ivantha@gmail.com>
2016-08-02 00:06:17 +0530

    no message

--- robofest_pi/main.py
+++ robofest_pi/main.py
@@ -7,14 +7,16 @@ cam = cv2.VideoCapture(0)       # Capture the feed from camera
 
 while True:
     ret, frame = cam.read()
+    return_frame = np.zeros(frame.shape, np.uint8)
     # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     # gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    return_frame = np.zeros(frame.shape, np.uint8)
 
 
 
 
-    box.detect_box(frame, return_frame)
+
+
+
 
 
 
